6d8d2ca61b2477aabbac9aa7fb43a11c5c88b7dd
Dmitry Weiner <dmitry.weiner@gmail.com>
2018-12-04 22:15:51 +0700

    Create food class

--- js/food.js
+++ js/food.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// TODO
+function Food(x, y) {
+    SimpleObject.call(this, x, y); // parent constructor
+    this.size = Creature.prototype.SIZE;
+    this.sign = getEmojiForFood();
+}
+
+Food.prototype = Object.create(SimpleObject.prototype, {
+    SIZE: {
+        value: 20,
+        enumerable: true,
+        configurable: true,
+        writable: false
+    },
+    createDOMElement: {
+        value: function() {
+            const element = document.createElement("div");
+            const gameField = document.getElementById(GAME_FIELD_ID);
+            element.setAttribute("id", this.id);
+            element.innerHTML = this.sign;
+            element.setAttribute("class", "food");
+            gameField.appendChild(element);
+        },
+        enumerable: true,
+        configurable: true,
+        writable: true
+    },
+});
+
+Food.prototype.constructor = Food;
+
--- js/utils.js
+++ js/utils.js
@@ -27,11 +27,35 @@ function getEmojiForCreature() {
         '\u{1F986}',
         '\u{1F41E}',
         '\u{1F577}',
+        '\u{1F980}',
+        '\u{1F980}',
+        '\u{1F990}',
+        '\u{1F991}',
+        '\u{1F41C}',
     ];
 
     return emoji[Math.round(Math.random() * (emoji.length - 1))];
 }
 
+/**
+ * Returns food emoji
+ * @returns {string}
+ */
 function getEmojiForFood() {
+    const emoji = [
+        '\u{1F966}',
+        '\u{1F344}',
+        '\u{1F955}',
+        '\u{1F353}',
+        '\u{1F352}',
+        '\u{1F350}',
+        '\u{1F34F}',
+        '\u{1F34E}',
+        '\u{1F34D}',
+        '\u{1F34C}',
+        '\u{1F349}',
+        '\u{1F347}',
+    ];
 
+    return emoji[Math.round(Math.random() * (emoji.length - 1))];
 }
