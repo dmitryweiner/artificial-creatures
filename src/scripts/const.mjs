2094012da3c2b67ce58e6be2ba07aeca31c2a86a
dmitryweiner <dmitry.weiner@gmail.com>
2019-11-11 23:58:12 +0700

    Minor changes

--- src/scripts/const.mjs
+++ src/scripts/const.mjs
@@ -3,7 +3,7 @@ export const DELAY = 10;
 export const STATISTICS_DELAY = 250;
 export const MAX_TTL = 6000;
 export const FOOD_TTL_REWARD = MAX_TTL * 1.5;
-export const FOOD_RATE_COEFFICIENT = 0.6;
+export const FOOD_RATE_COEFFICIENT = 0.5;
 export const POPULATION_SIZE = 100;
 export const ELITISM = POPULATION_SIZE * 0.4;
 export const MUTATION_RATE = 0.3;
@@ -12,7 +12,7 @@ export const TRAINING_CELL_SIZE = 2000;
 export const MAX_SEEKING_DISTANCE = TRAINING_CELL_SIZE * 0.25;
 export const SECTORS_OF_VISION = 16;
 export const SCORE_REPRODUCTION_COEFFICIENT = 10; // offspring for highest score
-export const OVERHEAD = 3;
+export const OVERHEAD = 2;
 
 export const FOUND_FOOD_SCORE = 10;
 export const CLOSER_TO_FOOD_COEFFICIENT = 0;
